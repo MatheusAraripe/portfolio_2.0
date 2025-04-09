a8d08295ec0862e682378b3c78f31890cbc89c40
MatheusAraripe <matheus.alc2001@gmail.com>
2025-04-09 14:49:51 -0300

    feat: adicionando foto e configurando css global

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,24 @@
+import { styled } from "@mui/material"
+import Avatar from "../assets/foteca_portfolio_redi.jpg"
+
 const Home = () => {
+
+    const StyledHome = styled("div")(() => ({
+        backgroundColor: 'black',
+        height: '100vh'
+    }))
+    const StyledAvatar = styled("img")(() => ({
+        borderRadius: '20px',
+        width: '350px',
+        height: '400px'
+
+    }))
   return (
-    <p>
-      PORTFOLIO Matheus
-    </p>
+    <>
+        <StyledHome>
+            <StyledAvatar src={Avatar} />
+        </StyledHome>
+    </>
   )
 }
 
